ed987ebe289e2ee6224dae1541d0abbf9a6cc305
Caio Sain Vallio <caiosainvallio@me.com>
2021-10-27 08:06:28 -0300

    Add files via upload

---
+++ notas_aula.R
@@ -0,0 +1,53 @@
+
+# dataset
+data(mtcars)
+
+
+####################### reg linear #########################
+# rstanarm -------------------------------------------------
+# r
+# stan
+# applyed regression models
+
+library(rstanarm)
+
+m1 <- stan_glm(mpg ~ hp + carb + gear, data=mtcars)
+summary(m1)
+plot(m1)
+
+
+
+
+# brms ----------------------------------------------------
+# bayesian
+# regression
+# models
+# using Stan
+
+library(brms)
+# file.edit(file.path(Sys.getenv("HOME"), ".R", "Makevars.win"))
+# excluir duplicatas
+
+m2 <- brm(mpg ~ hp + carb + gear, data=mtcars)
+summary(m2)
+plot(m2)
+
+
+
+
+####################### reg binomial #########################
+m3 <- brm(am ~ hp + cyl + carb, data = mtcars, family = bernoulli)
+summary(m3)
+plot(m3)
+
+
+
+##################### reg binomial (mixed) ######################
+m4 <- brm(am ~ hp + cyl + carb + (1|am), data = mtcars, family = bernoulli)
+summary(m4)
+plot(m4)
+pairs(m4)
+
+
+
+
