f82e862ac7cfb990ad7bcbc497a5cdda7714ae3d
Caio Sain Vallio <caiosainvallio@me.com>
2021-10-28 08:54:41 -0300

    Add files via upload

--- regressao_generalizada.R
+++ regressao_generalizada.R
@@ -49,4 +49,41 @@ odds2prob(exp(m2$coefficients))
 
 
 
+############################# brms ###############################
+# pacotes
+library(brms)
+
+# multi-threading
+options(mc.cores = parallel::detectCores())
+
+# modelo 3
+m3 <- brm(
+  switch ~ arsenic + dist_km + assoc + educ,
+  data = wells,
+  family = binomial(link = "logit"),
+  prior = c(
+    set_prior("normal(0, 2.5)", class = "b"),
+    set_prior("normal(0, 2.5)", class = "Intercept")
+  )
+)
+summary(m3)
+
+
+# modelo 4
+m4 <- brm(
+  switch ~ arsenic + dist_km + assoc + educ,
+  data = wells,
+  family = bernoulli(link = "logit"),
+  prior = c(
+    set_prior("normal(0, 2.5)", class = "b"),
+    set_prior("normal(0, 2.5)", class = "Intercept")
+  )
+)
+summary(m4)
+plot(m4)
+fixef(m4)
+exp(fixef(m4))
+odds2prob(exp(fixef(m4)))
+pp_check(m4, ndraws = 50)
+
 
