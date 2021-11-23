1860831c829c7e8d8085285a391bdec1e3a7b6a6
Caio Sain Vallio <caiosainvallio@me.com>
2021-11-23 15:01:24 -0300

    Add files via upload

--- regressao_poisson.R
+++ regressao_poisson.R
@@ -19,11 +19,35 @@
 # exposure2: numero de dias de exposicao a detetizacao
 
 library(rstanarm)
+library(ggplot2)
+library(dplyr)
 options(mc.cores = parallel::detectCores())
 data("roaches")
 
 
+# vizualizar a variavel dependente
+roaches %>% 
+  ggplot(aes(y)) + 
+  geom_histogram()
 
 
+sd(roaches$roach1) %>% log
+sd(roaches$treatment) %>% log
+sd(roaches$senior) %>% log
+
+sum(roaches$y == 0)
+log(0)
+
+
+m1 <- stan_glm(
+  y ~ roach1 + treatment + senior,
+  data = roaches,
+  family = poisson(link='log'),
+  prior = normal(c(0, 0, 0), c(0.1, 5, 5)),
+  prior_intercept = normal(0, 2.5)
+)
+
+summary(m1)
+pp_check(m1) # esta errando muito, ele nao consegue estimar o zero
 
 
