5f74fdf59dcca37512cfea0ba05e94470859993b
Caio Sain Vallio <caiosainvallio@me.com>
2021-11-23 15:30:21 -0300

    Add files via upload

---
+++ regressao_robusta.R
@@ -0,0 +1,18 @@
+############################################################################
+#                      Regressao Robusta                                   #
+############################################################################
+
+# usadas para:
+## outliers
+## Super(sub)dispersao - zero inflated
+
+# exemplos
+## t-Student <- Normal
+## Beta_binomial <- Binomial
+## t-Student <- Binomial
+## Binomial-negativa <- Poisson
+## Mistura de Binomial-negativa <- Poisson
+
+
+
+
