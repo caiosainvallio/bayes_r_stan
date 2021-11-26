b5e8c71447d2f502e64a8cd79145f51d42e3cf53
Caio Sain Vallio <caiosainvallio@me.com>
2021-11-25 22:27:54 -0300

    Add files via upload

--- modelos_multiniveis.R
+++ modelos_multiniveis.R
@@ -39,4 +39,46 @@ brm(
 
 
 
+# PRATICA ----------------------------------------------------------------------
+library(brms)
+library(tidyverse)
+options(mc.cores = parallel::detectCores())
+
+
+URL <- "https://raw.githubusercontent.com/storopoli/Estatistica-Bayesiana/master/datasets/cheese.csv"
+cheese <- read_csv2(URL, col_types = "fffi")
+
+cheese
+
+cheese$y %>% median
+cheese$y %>% mad
+1/cheese$y %>% median
+
+
+
+
+m1 <- brm(
+  y ~ (1 | cheese) + background,
+  data = cheese,
+  family = gaussian(link="identity"),
+  prior = c(
+    set_prior("normal(0, 1)", class="b"),
+    set_prior("student_t(3, 72, 13)", class="Intercept"),
+    set_prior("exponential(0.05)", class="sigma"),
+    set_prior("student_t(3, 0, 13)", class="sd", group="cheese")
+  )
+)
+
+summary(m1)
+pp_check(m1, ndraws = 50) + xlim(0, 200)
+
+
+
+
+
+
+
+
+
+
 
