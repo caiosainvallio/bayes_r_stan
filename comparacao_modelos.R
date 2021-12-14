123b232f3348580b9f8fe94a58b232cb943ef9fc
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-14 09:55:31 -0300

    Add files via upload

--- comparacao_modelos.R
+++ comparacao_modelos.R
@@ -12,3 +12,47 @@
 
 # psis-loo = pareto smoothed importance sampling loo
 
+# bibliotecas
+library(brms)
+options(mc.cores = parallel::detectCores())
+
+# dataset
+roaches <- rstanarm::roaches
+
+m1 <- brm(y ~ roach1 + treatment + senior,
+          data = roaches,
+          family = poisson,
+          prior = c(
+            set_prior("normal(0, 0.1)", class = "b", coef = "roach1"),
+            set_prior("normal(0, 5)", class = "b", coef = "treatment"),
+            set_prior("normal(0, 5)", class = "b", coef = "senior"),
+            set_prior("normal(0, 2.5)", class = "Intercept")
+          ))
+summary(m1)
+
+
+m2 <- brm(y ~ roach1 + treatment + senior,
+          data = roaches,
+          family = negbinomial,
+          prior = c(
+            set_prior("normal(0, 0.1)", class = "b", coef = "roach1"),
+            set_prior("normal(0, 5)", class = "b", coef = "treatment"),
+            set_prior("normal(0, 5)", class = "b", coef = "senior"),
+            set_prior("normal(0, 2.5)", class = "Intercept"),
+            set_prior("exponential(1)", class = "shape")
+          ))
+summary(m2)
+
+
+m3 <- brm(y ~ roach1 + treatment + senior,
+          data = roaches,
+          family = zero_inflated_negbinomial,
+          prior = c(
+            set_prior("normal(0, 0.1)", class = "b", coef = "roach1"),
+            set_prior("normal(0, 5)", class = "b", coef = "treatment"),
+            set_prior("normal(0, 5)", class = "b", coef = "senior"),
+            set_prior("normal(0, 2.5)", class = "Intercept"),
+            set_prior("exponential(1)", class = "shape"),
+            set_prior("beta(1, 1)", class = "zi")
+          ))
+summary(m3)
