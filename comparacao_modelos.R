1ec8c0b30027f6e7467c61aa34c5a1fa087baa4c
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-14 09:15:30 -0300

    Add files via upload

--- comparacao_modelos.R
+++ comparacao_modelos.R
@@ -6,7 +6,9 @@
 
 # elpd = expected log pointwise predictive density
 
-# loo = leave one out
+# loo = leave one out (cross-validation)
 
 # waic = widely applicable information criteria
+
+# psis-loo = pareto smoothed importance sampling loo
 
