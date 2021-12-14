8f38f28dea48c25b38def31a3b248ee5489582e0
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-14 09:09:19 -0300

    Add files via upload

--- comparacao_modelos.R
+++ comparacao_modelos.R
@@ -2,5 +2,11 @@
 #                      comparacao de modelos                               #
 ############################################################################
 
+# lppd = log posterior predictive density
+
 # elpd = expected log pointwise predictive density
+
+# loo = leave one out
+
+# waic = widely applicable information criteria
 
