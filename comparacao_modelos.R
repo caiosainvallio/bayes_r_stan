4a8ae1b1a218304a610731c950ad11bdc07c0f2e
Caio Sain Vallio <caiosainvallio@me.com>
2021-12-15 07:37:24 -0300

    Update comparacao_modelos.R

--- comparacao_modelos.R
+++ comparacao_modelos.R
@@ -3,13 +3,9 @@
 ############################################################################
 
 # lppd = log posterior predictive density
-
 # elpd = expected log pointwise predictive density
-
 # loo = leave one out (cross-validation)
-
 # waic = widely applicable information criteria
-
 # psis-loo = pareto smoothed importance sampling loo
 
 # bibliotecas
